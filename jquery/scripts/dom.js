9759bea60d521fd1c5cbe962acd617444014b9ee
Lucas <stick.goal@163.com>
2016-09-06 17:34:30 +0800

    增加jquery的dom操作部分

---
+++ jquery/scripts/dom.js
@@ -0,0 +1,30 @@
+$(function () {
+   //新增
+    $("#add").click(function () {
+        var item = $('<li class="item"></li>');
+        item.append($('<input type="text" name="item"> ').val($('ul li').length));
+        item.append($('<span class="close">&times;</span><span class="up">上</span> <span class="down">下</span>'));
+        item.insertBefore($("li:last"));
+    });
+    // 删除
+    $("#app").on("click",".close",function () {
+        $(this).parents("li").remove();
+    });
+    //up
+    $("#app").on("click",".up",function () {
+        var currItem=$(this).parents("li");
+        var prevItem =currItem.prev('.item');
+        if(prevItem.length){
+            currItem.insertBefore(prevItem);
+        }
+    });
+    //down
+    $("#app").on("click",".down",function () {
+        var currItem=$(this).parents("li");
+        var nextItem =currItem.next('.item');
+        if(nextItem.length){
+            currItem.insertAfter(nextItem);
+        }
+    });
+
+});
